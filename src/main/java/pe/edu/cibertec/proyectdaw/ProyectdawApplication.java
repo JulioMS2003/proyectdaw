cf395b5e0bc23d2e57cf403e2c532604772658a7
JulioMS2003 <julimanchay4862@gmail.com>
2024-04-21 00:15:54 -0500

    Agregando Proyecto

---
+++ src/main/java/pe/edu/cibertec/proyectdaw/ProyectdawApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.proyectdaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectdawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectdawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/proyectdaw/ProyectdawApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.proyectdaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectdawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
